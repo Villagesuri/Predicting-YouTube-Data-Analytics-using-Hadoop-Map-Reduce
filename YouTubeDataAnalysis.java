cbfb7917eb2c27d9d2eb737570ef872a016e1713
R.Jeya surya <167198539+Villagesuri@users.noreply.github.com>
2025-12-01 12:04:47 +0530

    Create YouTubeDataAnalysis.java

---
+++ YouTubeDataAnalysis.java
@@ -0,0 +1,20 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+public class YouTubeDataAnalysis {
+ public static void main(String[] args) throws Exception {
+ Configuration conf = new Configuration();
+ Job job = Job.getInstance(conf, "YouTube Data Analytics");
+ job.setJarByClass(YouTubeDataAnalysis.class);
+ job.setMapperClass(YouTubeMapper.class);
+ job.setReducerClass(YouTubeReducer.class);
+ job.setOutputKeyClass(Text.class);
+ job.setOutputValueClass(IntWritable.class);
+ FileInputFormat.addInputPath(job, new Path(args[0]));
+ FileOutputFormat.setOutputPath(job, new Path(args[1]));
+ System.exit(job.waitForCompletion(true) ? 0 : 1);
+ }
+}
